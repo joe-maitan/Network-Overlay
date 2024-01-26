09dbe9a98dc5d32b35ea287e8af5bf897c1a2f7a
jjmaitan <jjmaitan@colostate.edu>
2024-01-26 12:59:38 -0700

    Working on hw1

--- src/main/java/csx55/overlay/transport/TCPReceiverThread.java
+++ src/main/java/csx55/overlay/transport/TCPReceiverThread.java
@@ -9,26 +9,26 @@ public class TCPReceiverThread implements Runnable {
 
     public TCPReceiverThread(Socket socket) throws IOException {
         this.socket = socket;
-        data_in = new DataInputStream();
+        data_in = new DataInputStream(this.socket.getInputStream());
     } // End TCPRecieverThread(socket) constructor
 
     public void run() {
-    //     int data_length;
+        int data_length;
 
-    //     while (socket != null) {
-    //         try {
-    //             data_length = data_in.readInt();
+        while (socket != null) {
+            try {
+                data_length = data_in.readInt();
 
-    //             byte[] data = new byte[data_length];
-    //             data_in.readFully(data, 0, data_length);
-    //         } catch (SocketException se) {
-    //             System.out.println("SocketException - TCPRecieverThread class - SocketException - public void run().\n");
-    //             break;
-    //         } catch (IOException ioe) {
-    //             System.out.println("IOException - TCPRecieverThread class - IOException - public void run().\n");
-    //             break;
-    //         } // End try-catch block
-    //     } // End while loop
+                byte[] data = new byte[data_length];
+                data_in.readFully(data, 0, data_length);
+            } catch (SocketException se) {
+                System.out.println("SocketException - TCPRecieverThread class - SocketException - public void run().\n");
+                break;
+            } catch (IOException ioe) {
+                System.out.println("IOException - TCPRecieverThread class - IOException - public void run().\n");
+                break;
+            } // End try-catch block
+        } // End while loop
     } // End run() method
 
 } // End TCPReceiverThread class
