1729148767d53e48140e181b66a14a24ce9c5390
jjmaitan <jjmaitan@colostate.edu>
2024-01-31 12:54:48 -0700

    Working on hw1

--- src/main/java/csx55/overlay/wireformats/Register.java
+++ src/main/java/csx55/overlay/wireformats/Register.java
@@ -1,12 +1,16 @@
 package csx55.overlay.wireformats;
+
 import csx55.overlay.node.*;
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
 
 public class Register implements Event {
 
-    // We marshall these fields
-    int message_type;
     String IP_address;
     int port_number;
+    int message_type;
 
     public Register(MessagingNode new_msg_node) {
         this.IP_address = new_msg_node.mn_ip_address;
@@ -16,19 +20,37 @@ public class Register implements Event {
     
     @Override
     public int getType() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getType'");
-    }
+        return this.message_type;
+        // throw new UnsupportedOperationException("Unimplemented method 'getType'");
+    } // End getType() method
+
     @Override
     public byte[] getBytes() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getBytes'");
-    }
+        byte[] marshalledBytes = null;
+        ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+
+        try {
+            dout.writeInt(port_number);
+            dout.writeChars(IP_address);
+
+            marshalledBytes = baOutputStream.toByteArray();
+            baOutputStream.close();
+
+            dout.flush();
+            dout.close();
+        } catch (IOException err) {
+            System.err.println(err.getMessage());
+        }
+
+        return marshalledBytes;
+    } // End getBytes() method
+
     @Override
     public void setBytes(byte[] arr) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setBytes'");
-    }
+    } // End setBytes() method
 
 
 } // End Register class
