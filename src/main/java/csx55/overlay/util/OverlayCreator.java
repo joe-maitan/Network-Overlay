097751279a9bc837b45a2838f8633c739a871334
jjmaitan <jjmaitan@colostate.edu>
2024-02-04 22:33:32 -0700

    added comments to util file

--- src/main/java/csx55/overlay/util/OverlayCreator.java
+++ src/main/java/csx55/overlay/util/OverlayCreator.java
@@ -2,4 +2,4 @@ package csx55.overlay.util;
 
 public class OverlayCreator {
     
-}
+} // End OverlayCreator class
--- src/main/java/csx55/overlay/util/StatisticsCollectorAndDisplay.java
+++ src/main/java/csx55/overlay/util/StatisticsCollectorAndDisplay.java
@@ -2,4 +2,4 @@ package csx55.overlay.util;
 
 public class StatisticsCollectorAndDisplay {
     
-}
+} // End StatisticsCollectoryAndDisplay class
